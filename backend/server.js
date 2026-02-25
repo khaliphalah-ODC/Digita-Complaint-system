00728c4e6ad223130cd2cc1b6554ffd46754c182
Yeato-y <yyeato@gmail.com>
2026-02-25 14:07:18 -0600

    committing complaint table

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import './src/model/connect.js';
 
 import userRoutes from './src/routes/user.route.js';
 import accessmentRoutes from './src/routes/accessment.route.js';
+
+
 import { CreateUsersTable } from './src/controllers/user.controller.js';
 import { CreateAccessmentsTable } from './src/controllers/accessment.controller.js';
 
@@ -16,8 +18,12 @@ import { createOrganizationTable } from './src/controllers/organization.controll
 import { DepartmentRouter } from './src/routes/department.route.js';
 import { createDepartmentTable } from './src/controllers/department.controller.js';
 
+import { ComplaintRouter } from './src/routes/complaint.route.js';
+import { createComplaintTable } from './src/controllers/complaint.controller.js';
 
-import StatusLogRouter from './src/routes/statusLog.route.js';
+
+
+
 
 dotenv.config();
 
@@ -27,10 +33,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', StatusLogRouter);
 
-app.use(cors());
-app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Digital Complaint Management System API');
@@ -48,6 +51,9 @@ const PORT = process.env.PORT || 5000;
 // Organization Route
 createOrganizationTable;
 app.use("/api/organization", OrganizationRouter);
+
+createComplaintTable;
+app.use("/api/complaint", ComplaintRouter);
 
 // Department Route
 createDepartmentTable;
