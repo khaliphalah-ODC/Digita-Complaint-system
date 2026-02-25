a6fc5afc9b3f541915352e92aa1365040ff8a0d9
Yeato-y <yyeato@gmail.com>
2026-02-25 12:59:43 -0600

    corrections on the naming convention

--- backend/src/model/department.model.js
+++ backend/src/model/department.model.js
@@ -5,12 +5,12 @@ export const Department = `CREATE TABLE IF NOT EXISTS department (
    description TEXT,
    accessmentId INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   FOREIGN KEY (organizationId) REFERENCES organization(organizationId),
-   FOREIGN KEY (accessmentId) REFERENCES accessment(accessmentId)
+   FOREIGN KEY (organizationId) REFERENCES organization(organization_id),
+   FOREIGN KEY (accessmentId) REFERENCES accessment(id)
 
 )`;
 
-export const insertDepartment = `INSERT INTO department (organizationId, name, description, accessmentId) VALUES (?, ?, ?, ?)`;
+export const insertDepartment = `INSERT INTO department (organization_id,name, description, accessmentId) VALUES (?, ?, ?, ?)`;
 
 export const selectDepartments = `SELECT * FROM department`;
 
@@ -18,6 +18,6 @@ export const selectDepartmentById = `SELECT * FROM department WHERE departmentId
 
 export const deleteDepartmentById = `DELETE FROM department WHERE departmentId= ?`;
 
-export const updateDepartmentById = `UPDATE department SET organizationId = ?, name = ?, description = ?, accessmentId = ?, departmentId = ? WHERE departmentId = ?`;
+export const updateDepartmentById = `UPDATE department SET organization_id = ?, name = ?, description = ?, accessmentId = ?, departmentId = ? WHERE departmentId = ?`;
 
 
